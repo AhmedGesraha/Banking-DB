1423e1fe5f2e231820602e9b70959d2332c4638f
noahorensa <noahorensa@gmail.com>
2017-04-16 18:46:56 +0200

    Add files via upload

--- mysql/create_db.sql
+++ mysql/create_db.sql
@@ -3,11 +3,11 @@
 CREATE DATABASE bank;
 
 
---tables
+-- tables
 
 CREATE TABLE IF NOT EXISTS bank.client (
-	id INT NOT NULL,
-	status INT DEFAULT 1,
+	id VARCHAR(30) NOT NULL,
+	status INT NOT NULL DEFAULT 1,
 	dateOfBirth DATE NOT NULL,
 	username VARCHAR(40),
 	password VARCHAR(256),
@@ -15,8 +15,7 @@ CREATE TABLE IF NOT EXISTS bank.client (
 	phone VARCHAR(20),
 	salary INT,
 	fname VARCHAR(20) NOT NULL,
-	lname VARCHAR(20) NOT NULL,
-	cardNum INT
+	lname VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bank.savings_certificate (
@@ -27,15 +26,16 @@ CREATE TABLE IF NOT EXISTS bank.savings_certificate (
 	rate DOUBLE NOT NULL,
 	periodsPerYear INT NOT NULL,
 	currency CHAR(3) NOT NULL,
-	id INT,
+	id VARCHAR(30),
 	accountNum INT
 );
 
 CREATE TABLE IF NOT EXISTS bank.credit_card (
-	cardNum INT NOT NULL,
+	cardNum CHAR(16) NOT NULL,
 	max DOUBLE NOT NULL,
 	currency CHAR(3) NOT NULL,
-	pin VARCHAR(256) NOT NULL
+	pin VARCHAR(256) NOT NULL,
+	id VARCHAR(30)
 );
 
 CREATE TABLE IF NOT EXISTS bank.savings_account (
@@ -46,8 +46,8 @@ CREATE TABLE IF NOT EXISTS bank.savings_account (
 	available DOUBLE NOT NULL,
 	currency CHAR(3) NOT NULL,
 	active BOOLEAN NOT NULL,
-	id INT NOT NULL,
-	cardNum INT
+	id VARCHAR(30) NOT NULL,
+	cardNum CHAR(16)
 );
 
 CREATE TABLE IF NOT EXISTS bank.loan (
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS bank.loan (
 	paid DOUBLE NOT NULL,
 	startDate DATE NOT NULL,
 	endDate DATE,
-	id INT NOT NULL
+	id VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bank.checking_account (
@@ -67,8 +67,8 @@ CREATE TABLE IF NOT EXISTS bank.checking_account (
 	available DOUBLE NOT NULL,
 	currency CHAR(3) NOT NULL,
 	active BOOLEAN NOT NULL,
-	id INT NOT NULL,
-	cardNum INT
+	id VARCHAR(30) NOT NULL,
+	cardNum CHAR(16)
 );
 
 CREATE TABLE IF NOT EXISTS bank.check (
@@ -81,13 +81,13 @@ CREATE TABLE IF NOT EXISTS bank.check (
 	amount DOUBLE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.dedit_card (
-	cardNum INT NOT NULL,
+CREATE TABLE IF NOT EXISTS bank.debit_card (
+	cardNum CHAR(16) NOT NULL,
 	pin VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bank.guarantees (
-	id INT NOT NULL,
+	id VARCHAR(30) NOT NULL,
 	loanNum INT NOT NULL
 );
 
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS bank.guaranteed_by (
 );
 
 CREATE TABLE IF NOT EXISTS bank.employee (
-	id INT NOT NULL,
+	id VARCHAR(30) NOT NULL,
 	username VARCHAR(40) NOT NULL,
 	password VARCHAR(256) NOT NULL,
 	fname VARCHAR(20) NOT NULL,
@@ -111,3 +111,86 @@ CREATE TABLE IF NOT EXISTS bank.employee (
 	salary INT NOT NULL,
 	branch INT NOT NULL
 );
+
+
+-- indexes
+
+ALTER TABLE bank.client
+	ADD PRIMARY KEY (id);
+
+ALTER TABLE bank.savings_certificate
+	ADD PRIMARY KEY (certificateNum);
+
+ALTER TABLE bank.credit_card
+	ADD PRIMARY KEY (cardNum);
+
+ALTER TABLE bank.savings_account
+	ADD PRIMARY KEY (accountNum);
+
+ALTER TABLE bank.loan
+	ADD PRIMARY KEY (loanNum);
+
+ALTER TABLE bank.checking_account
+	ADD PRIMARY KEY (accountNum);
+
+ALTER TABLE bank.check
+	ADD PRIMARY KEY (checkNum, accountNum);
+
+ALTER TABLE bank.debit_card
+	ADD PRIMARY KEY (cardNum);
+
+ALTER TABLE bank.guarantees
+	ADD PRIMARY KEY (id, loanNum);
+
+ALTER TABLE bank.guaranteed_by
+	ADD PRIMARY KEY (certificateNum, loanNum);
+
+ALTER TABLE bank.employee
+	ADD PRIMARY KEY (id);
+
+
+-- auto increments
+
+ALTER TABLE bank.savings_certificate
+	MODIFY certificateNum INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE bank.savings_account
+	MODIFY accountNum INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE bank.loan
+	MODIFY loanNum INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE bank.checking_account
+	MODIFY accountNum INT NOT NULL AUTO_INCREMENT;
+
+
+-- constraints
+
+ALTER TABLE bank.savings_certificate
+	ADD CONSTRAINT savings_certificate_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT savings_certificate_accountNum_FK FOREIGN KEY (accountNum) REFERENCES bank.savings_account (accountNum) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE bank.credit_card
+	ADD CONSTRAINT credit_card_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE bank.savings_account
+	ADD CONSTRAINT savings_account_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT savings_account_cardNum_FK FOREIGN KEY (cardNum) REFERENCES bank.debit_card (cardNum) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE bank.loan
+	ADD CONSTRAINT loan_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE bank.checking_account
+	ADD CONSTRAINT checking_account_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT checking_account_cardNum_FK FOREIGN KEY (cardNum) REFERENCES bank.debit_card (cardNum) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE bank.check
+	ADD CONSTRAINT check_accountNum_FK FOREIGN KEY (accountNum) REFERENCES bank.checking_account (accountNum) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE bank.guarantees
+	ADD CONSTRAINT guarantees_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE CASCADE ON UPDATE CASCADE,
+	ADD CONSTRAINT guarantees_loanNum_FK FOREIGN KEY (loanNum) REFERENCES bank.loan (loanNum) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE bank.guaranteed_by
+	ADD CONSTRAINT guaranteed_by_certificateNum_FK FOREIGN KEY (certificateNum) REFERENCES bank.savings_certificate (certificateNum) ON DELETE CASCADE ON UPDATE CASCADE,
+	ADD CONSTRAINT guaranteed_by_loanNum_FK FOREIGN KEY (loanNum) REFERENCES bank.loan (loanNum) ON DELETE CASCADE ON UPDATE CASCADE;
