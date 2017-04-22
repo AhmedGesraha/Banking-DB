3d2d6d1bbcaad74f0e62e843aa3d969567d8ce3d
noahorensa <noahorensa@gmail.com>
2017-04-22 21:17:07 +0200

    fixed defaults for date values

--- mysql/create_db.sql
+++ mysql/create_db.sql
@@ -1,11 +1,11 @@
 -- construct db
 
 CREATE DATABASE bank;
-
+USE bank;
 
 -- tables
 
-CREATE TABLE IF NOT EXISTS bank.client (
+CREATE TABLE IF NOT EXISTS client (
 	id VARCHAR(30) NOT NULL,
 	status INT NOT NULL DEFAULT 1,
 	dateOfBirth DATE NOT NULL,
@@ -18,58 +18,58 @@ CREATE TABLE IF NOT EXISTS bank.client (
 	lname VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.savings_certificate (
+CREATE TABLE IF NOT EXISTS savings_certificate (
 	certificateNum INT NOT NULL,
-	startDate DATE NOT NULL DEFAULT CURDATE(),
-	amount DOUBLE NOT NULL,
+	startDate DATE NOT NULL,
+	amount DOUBLE PRECISION NOT NULL,
 	typeID INT NOT NULL,
 	id VARCHAR(30),
 	accountNum INT
 );
 
-CREATE TABLE IF NOT EXISTS bank.certificate_type (
+CREATE TABLE IF NOT EXISTS certificate_type (
 	typeId INT NOT NULL,
 	duration INT NOT NULL,
-	rate DOUBLE NOT NULL,
+	rate DOUBLE PRECISION NOT NULL,
 	periodsPerYear INT NOT NULL,
 	currency CHAR(3) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.credit_card (
+CREATE TABLE IF NOT EXISTS credit_card (
 	cardNum CHAR(16) NOT NULL,
-	max DOUBLE NOT NULL,
+	max DOUBLE PRECISION NOT NULL,
 	currency CHAR(3) NOT NULL,
 	pin VARCHAR(256) NOT NULL,
 	id VARCHAR(30)
 );
 
-CREATE TABLE IF NOT EXISTS bank.savings_account (
+CREATE TABLE IF NOT EXISTS savings_account (
 	accountNum INT NOT NULL,
-	issueDate DATE NOT NULL DEFAULT CURDATE(),
-	rate DOUBLE NOT NULL,
-	balance DOUBLE NOT NULL DEFAULT 0,
-	available DOUBLE NOT NULL DEFAULT 0,
+	issueDate DATE NOT NULL,
+	rate DOUBLE PRECISION NOT NULL,
+	balance DOUBLE PRECISION NOT NULL DEFAULT 0,
+	available DOUBLE PRECISION NOT NULL DEFAULT 0,
 	currency CHAR(3) NOT NULL,
 	active BOOLEAN NOT NULL DEFAULT 1,
 	id VARCHAR(30) NOT NULL,
 	cardNum CHAR(16)
 );
 
-CREATE TABLE IF NOT EXISTS bank.loan (
+CREATE TABLE IF NOT EXISTS loan (
 	loanNum INT NOT NULL,
-	amount DOUBLE NOT NULL,
-	rate DOUBLE NOT NULL,
-	paid DOUBLE NOT NULL,
-	startDate DATE NOT NULL DEFAULT CURDATE(),
+	amount DOUBLE PRECISION NOT NULL,
+	rate DOUBLE PRECISION NOT NULL,
+	paid DOUBLE PRECISION NOT NULL,
+	startDate DATE NOT NULL,
 	endDate DATE,
 	id VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.checking_account (
+CREATE TABLE IF NOT EXISTS checking_account (
 	accountNum INT NOT NULL,
-	issueDate DATE NOT NULL DEFAULT CURDATE(),
-	balance DOUBLE NOT NULL DEFAULT 0,
-	available DOUBLE NOT NULL DEFAULT 0,
+	issueDate DATE NOT NULL,
+	balance DOUBLE PRECISION NOT NULL DEFAULT 0,
+	available DOUBLE PRECISION NOT NULL DEFAULT 0,
 	currency CHAR(3) NOT NULL,
 	active BOOLEAN NOT NULL DEFAULT 1,
 	id VARCHAR(30) NOT NULL,
@@ -81,29 +81,29 @@ CREATE TABLE IF NOT EXISTS bank.check (
 	accountNum INT NOT NULL,
 	redeemer_fname VARCHAR(20) NOT NULL,
 	redeemer_lname VARCHAR(20) NOT NULL,
-	redeemDate DATE NOT NULL DEFAULT CURDATE(),
+	redeemDate DATE NOT NULL,
 	writingDate DATE NOT NULL,
-	amount DOUBLE NOT NULL
+	amount DOUBLE PRECISION NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.debit_card (
+CREATE TABLE IF NOT EXISTS debit_card (
 	cardNum CHAR(16) NOT NULL,
 	pin VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.guarantees (
+CREATE TABLE IF NOT EXISTS guarantees (
 	id VARCHAR(30) NOT NULL,
 	loanNum INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.guaranteed_by (
+CREATE TABLE IF NOT EXISTS guaranteed_by (
 	certificateNum INT NOT NULL,
 	loanNum INT NOT NULL,
 	state CHAR(1) NOT NULL,
 	dueDate DATE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank.employee (
+CREATE TABLE IF NOT EXISTS employee (
 	id VARCHAR(30) NOT NULL,
 	username VARCHAR(40) NOT NULL,
 	password VARCHAR(256) NOT NULL,
@@ -120,86 +120,138 @@ CREATE TABLE IF NOT EXISTS bank.employee (
 
 -- indexes
 
-ALTER TABLE bank.client
+ALTER TABLE client
 	ADD PRIMARY KEY (id);
 
-ALTER TABLE bank.savings_certificate
+ALTER TABLE savings_certificate
 	ADD PRIMARY KEY (certificateNum);
 
-ALTER TABLE bank.certificate_type
+ALTER TABLE certificate_type
 	ADD PRIMARY KEY (typeId);
 
-ALTER TABLE bank.credit_card
+ALTER TABLE credit_card
 	ADD PRIMARY KEY (cardNum);
 
-ALTER TABLE bank.savings_account
+ALTER TABLE savings_account
 	ADD PRIMARY KEY (accountNum);
 
-ALTER TABLE bank.loan
+ALTER TABLE loan
 	ADD PRIMARY KEY (loanNum);
 
-ALTER TABLE bank.checking_account
+ALTER TABLE checking_account
 	ADD PRIMARY KEY (accountNum);
 
 ALTER TABLE bank.check
 	ADD PRIMARY KEY (checkNum, accountNum);
 
-ALTER TABLE bank.debit_card
+ALTER TABLE debit_card
 	ADD PRIMARY KEY (cardNum);
 
-ALTER TABLE bank.guarantees
+ALTER TABLE guarantees
 	ADD PRIMARY KEY (id, loanNum);
 
-ALTER TABLE bank.guaranteed_by
+ALTER TABLE guaranteed_by
 	ADD PRIMARY KEY (certificateNum, loanNum);
 
-ALTER TABLE bank.employee
+ALTER TABLE employee
 	ADD PRIMARY KEY (id);
 
 
 -- auto increments
 
-ALTER TABLE bank.savings_certificate
+ALTER TABLE savings_certificate
 	MODIFY certificateNum INT NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE bank.savings_account
+ALTER TABLE savings_account
 	MODIFY accountNum INT NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE bank.loan
+ALTER TABLE loan
 	MODIFY loanNum INT NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE bank.checking_account
+ALTER TABLE checking_account
 	MODIFY accountNum INT NOT NULL AUTO_INCREMENT;
 
 
--- constraints
+-- foriegn key constraints
 
-ALTER TABLE bank.savings_certificate
-	ADD CONSTRAINT savings_certificate_typeId_FK FOREIGN KEY (typeId) REFERENCES bank.certificate_type (typeId) ON DELETE RESTRICT ON UPDATE CASCADE,
-	ADD CONSTRAINT savings_certificate_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	ADD CONSTRAINT savings_certificate_accountNum_FK FOREIGN KEY (accountNum) REFERENCES bank.savings_account (accountNum) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE savings_certificate
+	ADD CONSTRAINT savings_certificate_typeId_FK FOREIGN KEY (typeId) REFERENCES certificate_type (typeId) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT savings_certificate_id_FK FOREIGN KEY (id) REFERENCES client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT savings_certificate_accountNum_FK FOREIGN KEY (accountNum) REFERENCES savings_account (accountNum) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE bank.credit_card
-	ADD CONSTRAINT credit_card_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE credit_card
+	ADD CONSTRAINT credit_card_id_FK FOREIGN KEY (id) REFERENCES client (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE bank.savings_account
-	ADD CONSTRAINT savings_account_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	ADD CONSTRAINT savings_account_cardNum_FK FOREIGN KEY (cardNum) REFERENCES bank.debit_card (cardNum) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE savings_account
+	ADD CONSTRAINT savings_account_id_FK FOREIGN KEY (id) REFERENCES client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT savings_account_cardNum_FK FOREIGN KEY (cardNum) REFERENCES debit_card (cardNum) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE bank.loan
-	ADD CONSTRAINT loan_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE loan
+	ADD CONSTRAINT loan_id_FK FOREIGN KEY (id) REFERENCES client (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE bank.checking_account
-	ADD CONSTRAINT checking_account_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	ADD CONSTRAINT checking_account_cardNum_FK FOREIGN KEY (cardNum) REFERENCES bank.debit_card (cardNum) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE checking_account
+	ADD CONSTRAINT checking_account_id_FK FOREIGN KEY (id) REFERENCES client (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	ADD CONSTRAINT checking_account_cardNum_FK FOREIGN KEY (cardNum) REFERENCES debit_card (cardNum) ON DELETE SET NULL ON UPDATE CASCADE;
 
 ALTER TABLE bank.check
-	ADD CONSTRAINT check_accountNum_FK FOREIGN KEY (accountNum) REFERENCES bank.checking_account (accountNum) ON DELETE CASCADE ON UPDATE CASCADE;
+	ADD CONSTRAINT check_accountNum_FK FOREIGN KEY (accountNum) REFERENCES checking_account (accountNum) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE bank.guarantees
-	ADD CONSTRAINT guarantees_id_FK FOREIGN KEY (id) REFERENCES bank.client (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	ADD CONSTRAINT guarantees_loanNum_FK FOREIGN KEY (loanNum) REFERENCES bank.loan (loanNum) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE guarantees
+	ADD CONSTRAINT guarantees_id_FK FOREIGN KEY (id) REFERENCES client (id) ON DELETE CASCADE ON UPDATE CASCADE,
+	ADD CONSTRAINT guarantees_loanNum_FK FOREIGN KEY (loanNum) REFERENCES loan (loanNum) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE bank.guaranteed_by
-	ADD CONSTRAINT guaranteed_by_certificateNum_FK FOREIGN KEY (certificateNum) REFERENCES bank.savings_certificate (certificateNum) ON DELETE CASCADE ON UPDATE CASCADE,
-	ADD CONSTRAINT guaranteed_by_loanNum_FK FOREIGN KEY (loanNum) REFERENCES bank.loan (loanNum) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE guaranteed_by
+	ADD CONSTRAINT guaranteed_by_certificateNum_FK FOREIGN KEY (certificateNum) REFERENCES savings_certificate (certificateNum) ON DELETE CASCADE ON UPDATE CASCADE,
+	ADD CONSTRAINT guaranteed_by_loanNum_FK FOREIGN KEY (loanNum) REFERENCES loan (loanNum) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- triggers
+
+DELIMITER $
+CREATE TRIGGER savings_certificate_startDate_default 
+	BEFORE INSERT ON savings_certificate FOR EACH ROW
+	SET_DEF: BEGIN
+		IF (isnull(NEW.startDate)) THEN
+	 		SET NEW.startDate=CURDATE();
+	 	END IF;
+	END SET_DEF $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER savings_account_issueDate_default 
+	BEFORE INSERT ON savings_account FOR EACH ROW
+	SET_DEF: BEGIN
+		IF (isnull(NEW.issueDate)) THEN
+	 		SET NEW.issueDate=CURDATE();
+	 	END IF;
+	END SET_DEF $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER loan_startDate_default 
+	BEFORE INSERT ON loan FOR EACH ROW
+	SET_DEF: BEGIN
+		IF (isnull(NEW.startDate)) THEN
+	 		SET NEW.startDate=CURDATE();
+	 	END IF;
+	END SET_DEF $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER checking_account_issueDate_default 
+	BEFORE INSERT ON checking_account FOR EACH ROW
+	SET_DEF: BEGIN
+		IF (isnull(NEW.issueDate)) THEN
+	 		SET NEW.issueDate=CURDATE();
+	 	END IF;
+	END SET_DEF $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER check_redeemDate_default 
+	BEFORE INSERT ON bank.check FOR EACH ROW
+	SET_DEF: BEGIN
+		IF (isnull(NEW.redeemDate)) THEN
+	 		SET NEW.redeemDate=CURDATE();
+	 	END IF;
+	END SET_DEF $
+DELIMITER ;
