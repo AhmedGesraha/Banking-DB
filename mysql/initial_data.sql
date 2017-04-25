e4f91a0c8bb73118462bab3b9b55c0050106998d
noahorensa <noahorensa@gmail.com>
2017-04-25 19:33:43 +0200

    database inserts for initialization

---
+++ mysql/initial_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee(id, username, password, fname, lname, address, phone, dateOfBirth, type, salary, branch) VALUES ("111122223333", "bigboss", "bigbosspassword", "Big", "Boss", "123 Fake Street", "0123456789", "1955-10-18", 'm', 100000, 320);
+INSERT INTO employee(id, username, password, fname, lname, address, phone, dateOfBirth, type, salary, branch) VALUES ("222233334444", "customerserv", "pass", "John", "Smith", "124 Fake Street", "0123456789", "1955-10-18", 's', 100000, 320);
+INSERT INTO employee(id, username, password, fname, lname, address, phone, dateOfBirth, type, salary, branch) VALUES ("333344445555", "teller", "pass", "John", "Smiiiith", "125 Fake Street", "0123456789", "1955-10-18", 't', 100000, 320);
