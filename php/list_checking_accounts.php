10dfa3b1440818e51913b123d496a76845e475b7
noahorensa <noahorensa@gmail.com>
2017-05-08 00:51:17 +0200

    backend update
    echoing html instead of tuples

--- php/list_checking_accounts.php
+++ php/list_checking_accounts.php
@@ -15,10 +15,33 @@
 
 				$connection = mysqli_connect("localhost", "root", "", "bank");
 				
+				echo 
+					"<table class='table'>".
+						"<thead>".
+							"<tr>".
+								"<th class='col-sm-1'>Account number</th>".
+								"<th class='col-sm-1'>Currency</th>".
+								"<th class='col-sm-1'>Balance</th>".
+								"<th class='col-sm-1'>Available balance</th>".
+								"<th class='col-sm-1'>Active</th>".
+								"<th class='col-sm-10'>Issue date</th>".
+								"<th class='col-sm-1'>Linked debit card</th>".
+							"</tr>".
+						"</thead>"
+					;
+
 				$result = mysqli_query($connection, $query);
 				foreach ($result as $account)
 				{
-					echo "(".$account['accountNum'].",".$account['currency'].",".$account['balance'].",".$account['available'].",".$account['active'].",".$account['issueDate'].",".$account['cardNum'].")";
+					echo "<tr>";
+					echo "<td>".$account['accountNum']."</td>";
+					echo "<td>".$account['currency']."</td>";
+					echo "<td>".$account['balance']."</td>";
+					echo "<td>".$account['available']."</td>";
+					echo "<td>".$account['active']."</td>";
+					echo "<td>".$account['issueDate']."</td>";
+					echo "<td>".$account['cardNum']."</td>";
+					echo "</tr>";
 				}
 
 				mysqli_close($connection);
--- php/list_savings_accounts.php
+++ php/list_savings_accounts.php
@@ -16,9 +16,35 @@
 				$connection = mysqli_connect("localhost", "root", "", "bank");
 				
 				$result = mysqli_query($connection, $query);
+
+				echo 
+					"<table class='table'>".
+						"<thead>".
+							"<tr>".
+								"<th class='col-sm-1'>Account number</th>".
+								"<th class='col-sm-1'>Currency</th>".
+								"<th class='col-sm-1'>Rate</th>".
+								"<th class='col-sm-1'>Balance</th>".
+								"<th class='col-sm-1'>Available balance</th>".
+								"<th class='col-sm-1'>Active</th>".
+								"<th class='col-sm-10'>Issue date</th>".
+								"<th class='col-sm-1'>Linked debit card</th>".
+							"</tr>".
+						"</thead>"
+					;
+
 				foreach ($result as $account)
 				{
-					echo "(".$account['accountNum'].",".$account['currency'].",".$account['rate'].",".$account['balance'].",".$account['available'].",".$account['active'].",".$account['issueDate'].",".$account['cardNum'].")";
+					echo "<tr>";
+					echo "<td>".$account['accountNum']."</td>";
+					echo "<td>".$account['currency']."</td>";
+					echo "<td>".$account['rate']."</td>";
+					echo "<td>".$account['balance']."</td>";
+					echo "<td>".$account['available']."</td>";
+					echo "<td>".$account['active']."</td>";
+					echo "<td>".$account['issueDate']."</td>";
+					echo "<td>".$account['cardNum']."</td>";
+					echo "</tr>";
 				}
 
 				mysqli_close($connection);
